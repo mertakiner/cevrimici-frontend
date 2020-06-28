f8b887a24e1bb5e9fc07870a23c64e27cad5c955
mert <mac@Merts-iMac.local>
2020-06-28 13:32:25 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [ meetings, setMeetings ] = useState( [] );
+
+  const getMeetings = async () => {
+    try {
+      const response = await fetch("http://localhost:1337/etkinliks");
+      const jsonData = await response.json();
+
+      setMeetings(jsonData);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  useEffect( () => {
+    getMeetings();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        
+        
+        <tbody>
+          {meetings.map( (meet)=> (
+            <tr key={meet.id} >
+              <th scope="row">{meet.id}</th>
+              <td>{meet.Title} </td>
+              <td>{meet.Description}</td>
+              <td>{meet.Date}</td>
+            </tr>
+          ) 
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
